af0fe82b507d5431b79ea49a0d3f04ed7dc350aa
Luis Eugenio <luiseugeniomb@gmail.com>
2019-05-18 10:23:16 -0300

    Add API endpoints create => to start a new game play => with arguments x and y to play flag => with arguments x and y to flag status => current game state

---
+++ app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::GamesController < ApplicationController
+
+	def create
+		
+	end
+
+	def play
+		
+	end
+
+	def flag
+		
+	end
+
+	def board_status
+		
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+		namespace :v1 do
+		  controller :games do
+		    post '/minesweeper/create', to: 'games#create'
+		    patch '/minesweeper/play/:x/:y', to: 'games#play', as: :minesweeper_play
+		    patch '/minesweeper/flag/:x/:y', to: 'games#flag', as: :minesweeper_flag
+		    get '/minesweeper/status', to: 'games#board_status', as: :minesweeper_status
+		  end
+		end
+	end
+
 end
